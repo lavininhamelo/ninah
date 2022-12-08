3d6e41aa80d365e6f2c16ea10d7fc8edd53712eb
Lavinia Melo <lavininhamelo@hotmail.com>
2022-12-08 00:48:52 +0000

    feat: add github login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import 'styles/globals.css'
-import type { AppProps } from 'next/app'
+import "styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider, SessionProviderProps } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type Props = AppProps & { session: SessionProviderProps["session"] };
+
+export default function App({ Component, pageProps, session }: Props) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
